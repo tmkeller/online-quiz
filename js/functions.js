26f2aff92f207bbe45d2c00410f70ee132d4ab18
Tim Keller <timothy.m.keller@gmail.com>
2020-12-13 18:52:47 -0800

    Finished function to randomize and display questions and answers as header text and buttons, respectively.

--- js/functions.js
+++ js/functions.js
@@ -11,6 +11,38 @@ function buildQuestionList() {
     return arr;
 }
 
+// Displays the chosen question in the DOM. Written to randomize the location of the answer.
+function displayQuestion( index ) {
+    var quizQuestion = quizQuestionsArr[ index ];
+    var keysNum = Object.keys( quizQuestion );
+    var headerText = document.querySelector( "#header_text" );
+    var answerField = document.querySelector( "#answer_field" );
+    var answersArray = [];
+
+    // put all answers in answersArray so their order can be randomized without modifying the quizQuestion object.
+    for ( var i = 1; i < keysNum.length - 1; i++ ) {
+        answersArray.push( quizQuestion[ i ] );
+    }
+
+    // We start on answer #1.
+    var answerNum = 1;
+    while ( answersArray.length > 0 ) {
+        // Randomize the order in which answers appear.
+        var arrIndex = Math.floor( Math.random() * answersArray.length );
+        var newButton = document.createElement( "button" );
+        newButton.textContent = `${ answerNum }. ${answersArray[ arrIndex ]}`;
+        answerField.appendChild( newButton );
+        newButton.insertAdjacentHTML('afterend', '<br>');
+
+        answersArray.splice( arrIndex, 1 );
+        answerNum++;
+    }
+    console.log( answersArray );
+    console.log( `The answer is ${quizQuestion.answer}` );
+
+    headerText.textContent = quizQuestion.question;
+}
+
 function setTimer( timeSet ) {
     var timerSection = document.querySelector( "#timer_sec" );
     var timer = document.querySelector( "#time" );
@@ -33,3 +65,4 @@ function timerPenalty( penaltySeconds ) {
 }
 
 setTimer( timeSet );
+displayQuestion( 0 );
--- js/questions.js
+++ js/questions.js
@@ -1,32 +1,32 @@
 var q1 = {
     question: "A function that calls itself is _____.",
-    a: "recursive",
-    b: "a callback function",
-    c: "algorithmic",
-    d: "all of the above",
-    answer: "a"
+    1: "recursive",
+    2: "a callback function",
+    3: "algorithmic",
+    4: "schizophrenic",
+    answer: 1
 }
 var q2 = {
     question: "_____ invented the world wide web.",
-    a: "Tim Berners-Lee",
-    b: "William Henry Gates II",
-    c: "researchers at UC Berkley in 1989",
-    d: "Linus Torvalds",
-    answer: "a"
+    1: "Tim Berners-Lee",
+    2: "William Henry Gates II",
+    3: "researchers at UC Berkley in 1989",
+    4: "Linus Torvalds",
+    answer: 1
 }
 var q3 = {
     question: "A _____ is a function that has another function as a return value.",
-    a: "higher-order function",
-    b: "a callback function",
-    c: "first-order function",
-    d: "map function",
-    answer: "a"
+    1: "higher-order function",
+    2: "a callback function",
+    3: "first-order function",
+    4: "map function",
+    answer: 1
 }
 var q4 = {
     question: "The _____ is the tree of HTML elements in a window that comprise a web page.",
-    a: "DOM",
-    b: "API",
-    c: "URL",
-    d: "NAMBLA",
-    answer: "a"
+    1: "DOM",
+    2: "API",
+    3: "URL",
+    4: "NAMBLA",
+    answer: 1
 }
